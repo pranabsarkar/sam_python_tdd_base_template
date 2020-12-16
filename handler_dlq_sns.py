36d25a51f30ead5e6c3295ab7f27fa614ab79476
pranabsarkar <sarkarpranab66@gmail.com>
2020-12-16 14:20:59 +0530

    rough work

--- handler_dlq_sns.py
+++ handler_dlq_sns.py
@@ -1,23 +1,13 @@
 import json
 import boto3
-# import requests
-
 
 def handle(event, context):
     print(event)
 
-    # try:
-    #     ip = requests.get("http://checkip.amazonaws.com/")
-    # except requests.RequestException as e:
-    #     # Send some context about this error to Lambda Logs
-    #     print(e)
-
-    #     raise e
-
     messages = json.loads(event['Records'][0]['body'])
 
     try:
-        if messages[0]['user_id'] == "eb957e17-3ca8-11eb-9fc0-b068e63error" or messages[0]['user_id'] == "eb957e17-3ca8-11eb-9fc0-b068e63Error":
+        if messages[0]['user_id'] == "eb957e17-3ca8-11eb-9fc0-b068e63Error":
             raise TypeError("Error")
         else:
             dynamodb = boto3.resource('dynamodb')
@@ -28,9 +18,12 @@ def handle(event, context):
                     writer.put_item(Item=item)
     except Exception as e:
         print(e)
-        sqs = boto3.resource('sqs')
-        queue = sqs.get_queue_by_name(QueueName='SimpleQueuePocDlq')
-        response = queue.send_message(MessageBody=event['Records'][0]['body'])
+        sns = boto3.client('sns')
+        response = sns.publish(
+            TopicArn='arn:aws:sns:us-east-1:301598911115:SampleTopicPoc',    
+            Message=f"Dear Admin, the system is not able to process the import data please check - {messages}",
+            Subject='Data Import Process Failure', 
+        )
         print(response)
 
     return {
